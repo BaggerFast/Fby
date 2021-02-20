4032053c644729404dc6a5276cc540dcad7724dc
Stepan Fadeev <ftv3614@gmail.com>
2021-02-21 00:28:08 +0300

    Add getting total catalogue from YM and saving catalogue to data_file.json. Add cyrillic symbols to json.

--- main/views.py
+++ main/views.py
@@ -9,25 +9,51 @@ from main.models import Barcode
 
 
 def catalogue_list(request):
-    data = get_data_from_yandex()
+    json_object = get_catalogue_from_file("data_file.json")
 
     # TODO: save data to DB
-    json_object = json.loads(data)
-    save_to_db(json_object)
+    # save_to_db(json_object)
 
-    formatted_json = json.dumps(json_object, indent=2)
+    formatted_json = json.dumps(json_object, indent=2, ensure_ascii=False)
     colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.HtmlFormatter())
     context = {
         'highlight_style': formatters.HtmlFormatter().get_style_defs('.highlight'),
-        'content': colorful_json
+        'content': colorful_json,
     }
     return render(request, 'list.html', context)
 
 
-def get_data_from_yandex():
+def get_catalogue_from_ym():
+    """
+    Загрузка каталога из YandexMarket и сохранение в файл data_file.json
+    """
+    data = get_data_from_yandex()
+    json_object = json.loads(data)
+    while 'nextPageToken' in json_object['result']['paging']:  # если страница не последняя, читаем следующую
+        next_page_token = json_object['result']['paging']['nextPageToken']
+        next_json_object = json.loads(get_data_from_yandex(next_page_token))
+        json_object['result']['offerMappingEntries'] += next_json_object['result']['offerMappingEntries']
+        json_object['result']['paging'] = next_json_object['result']['paging']
+    with open("data_file.json", "w") as write_file:
+        json.dump(json_object, write_file, indent=2, ensure_ascii=False)
+    return json_object
+
+
+def get_catalogue_from_file(file):
+    """
+    Загрузка каталога из файла file
+    """
+    with open(file, "r") as read_file:
+        json_object = json.load(read_file)
+    return json_object
+
+
+def get_data_from_yandex(next_page_token=None):
     headers_str = f'OAuth oauth_token="{YA_MARKET_TOKEN}", oauth_client_id="{YA_MARKET_CLIENT_ID}"'
     headers = {'Authorization': headers_str}
     url = f'https://api.partner.market.yandex.ru/v2/campaigns/{YA_MARKET_SHOP_ID}/offer-mapping-entries.json'
+    if next_page_token:
+        url += f'?page_token={next_page_token}'
     data = requests.get(url, headers=headers)
     return data.content
 
