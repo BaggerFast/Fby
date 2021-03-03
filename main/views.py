58e5f18c55a624afae3db52d6f3ced04d2bab1c3
Igor Tatarinov <ig.tatarinov@gmail.com>
2021-03-03 11:44:15 +0200

    Организованно формирование каталога на основе данных из БД, а не JSON

--- main/views.py
+++ main/views.py
@@ -1,18 +1,20 @@
 import json
 import requests
+from django.core import serializers
 from django.shortcuts import render
 from pygments import highlight, lexers, formatters
 from fby_market.settings import YA_MARKET_TOKEN, YA_MARKET_CLIENT_ID, YA_MARKET_SHOP_ID
 from main.models.offer_save import OfferPattern
 from main.models.base import Offer
+from django.forms.models import model_to_dict
 
 
 def catalogue_list(request):
-    json_object = get_catalogue_from_file("data_file.json")
-    save_to_db(json_object)
+    data_objects = Offer.objects.all()
+    json_object = serializers.serialize('json', data_objects)
 
-    formatted_json = json.dumps(json_object, indent=2, ensure_ascii=False)
-    colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.HtmlFormatter())
+    json_object = json.dumps(json_object, sort_keys=True, indent=2, ensure_ascii=False)
+    colorful_json = highlight(json_object, lexers.JsonLexer(), formatters.HtmlFormatter())
     context = {
         'highlight_style': formatters.HtmlFormatter().get_style_defs('.highlight'),
         'content': colorful_json,
@@ -76,7 +78,10 @@ def get_data_from_yandex(next_page_token=None):
     data = requests.get(url, headers=headers)
     return data.content
 
-
 def save_to_db(data):
     data = OfferPattern(json=data['result']['offerMappingEntries'])
     data.save()
+
+def paginator(sku_codes):
+    for i in range(sku_codes.len()):
+        data_object = Offer.objects.get(skui)
