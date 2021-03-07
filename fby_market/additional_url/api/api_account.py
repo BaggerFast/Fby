edc2e459a85c16dc8b5bfa75cf62fea21d73e8d3
Daniil Aleksandrov <riosha3@gmail.com>
2021-03-07 15:32:35 +0300

    refix urls

--- fby_market/additional_url/api/api_account.py
+++ fby_market/additional_url/api/api_account.py
@@ -2,6 +2,6 @@ from main.views import account_login, account_register
 from django.urls import path
 
 urlpatterns = [
-    path('accounts/login/', account_login, name='account_login'),
-    path('account/register/', account_register, name='account_register'),
+    path('login/', account_login, name='account_login'),
+    path('register/', account_register, name='account_register'),
     ]
