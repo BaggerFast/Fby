a97612a3402580a775f70e82684b44e09fdb10b2
Igor Tatarinov <ig.tatarinov@gmail.com>
2021-03-07 14:08:15 +0200

    API для регистрации и логина

--- fby_market/settings.py
+++ fby_market/settings.py
@@ -52,8 +52,10 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 ROOT_URLCONF = 'fby_market.urls'
