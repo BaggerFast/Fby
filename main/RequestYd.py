6036540984bae0fefe1d186fef3c999bbc5947cf
Stepan Fadeev <ftv3614@gmail.com>
2021-03-17 19:51:38 +0300

    Improve class Requests, add post-requests to base class.

--- main/RequestYd.py
+++ main/RequestYd.py
@@ -1,3 +1,5 @@
+"""Формирование запросов в YM для получения данных и сохранения их в БД"""
+
 from fby_market.settings import YaMarket
 import requests
 from main.models.save_dir.offer import OfferPattern
@@ -5,60 +7,80 @@ from main.models.save_dir.prices import PricePattern
 
 
 class Requests:
-    def __init__(self, json_name):
-        self.url = f'https://api.partner.market.yandex.ru/v2/campaigns/{YaMarket.SHOP_ID}{json_name}.json'
-        self.headers_str = f'OAuth oauth_token=\"{YaMarket.TOKEN}\", oauth_client_id=\"{YaMarket.CLIENT_ID}\"'
+    """
+    Базовый класс для получения данных и сохранения в БД
+    """
+
+    PARAMS = None # параметры запроса в формате json (для post-запросов)
+
+    def __init__(self, json_name: str, base_context_name: str):
+        self.url = f'https://api.partner.market.yandex.ru/v2/campaigns/{YaMarket.SHOP_ID}/{json_name}.json'
+        self.headers_str = f'OAuth oauth_token="{YaMarket.TOKEN}", oauth_client_id="{YaMarket.CLIENT_ID}"'
         self.headers = {'Authorization': self.headers_str, 'Content-type': 'application/json'}
+        self.base_context_name = base_context_name  # название элемента во входном json, содержащего требуемые данные
         self.json_data = self.get_json()
 
-    def get_json(self, ):
-        data = requests.get(self.url, headers=self.headers)
-        json_data = data.json()
-        print(json_data)
+    def get_json(self):
+        """
+        Получение данных от YM
+        """
+        json_data = self.get_next_page()
         if "OK" in json_data['status']:
-            json_data = self.check_next_page(json_data=json_data)
+            json_data = self.get_all_pages(json_data=json_data)
         return json_data
 
-    def check_next_page(self, json_data):
-        raise NotImplementedError
-
     def get_next_page(self, next_page_token=None):
-        if next_page_token:
-            self.url += f'?page_token={next_page_token}'
-        data = requests.get(self.url, headers=self.headers)
+        """
+        Формирование запроса и получение очередной страницы данных
+        (если next_page_token не задан, вернется первая страница)
+        """
+
+        url = self.url + f'?page_token={next_page_token}' if next_page_token else self.url
+        if self.PARAMS:  # если есть входные параметры, формируем post-запрос
+            data = requests.post(url, headers=self.headers, json=self.PARAMS)
+        else:
+            data = requests.get(url, headers=self.headers)
         return data.json()
 
+    def get_all_pages(self, json_data):
+        """
+        Получение всех страниц данных
+        """
+
+        while 'nextPageToken' in json_data['result']['paging']:  # если страница не последняя, читаем следующую
+            next_page_token = json_data['result']['paging']['nextPageToken']
+            next_json_object = self.get_next_page(next_page_token)
+            json_data['result'][self.base_context_name] += next_json_object['result'][self.base_context_name]
+            json_data['result']['paging'] = next_json_object['result']['paging']
+        return json_data
+
     def save(self):
+        """
+        Сохранение данных в соответствующую БД
+        """
+
         raise NotImplementedError
 
 
 class OfferList(Requests):
-    def __init__(self):
-        super().__init__(json_name='/offer-mapping-entries')
+    """
+    Класс для получения списка товаров и сохранения в БД Offer
+    """
 
-    def check_next_page(self, json_data):
-        while 'nextPageToken' in json_data['result']['paging']:  # если страница не последняя, читаем следующую
-            next_page_token = json_data['result']['paging']['nextPageToken']
-            next_json_object = self.get_next_page(next_page_token)
-            json_data['result']['offerMappingEntries'] += next_json_object['result']['offerMappingEntries']
-            json_data['result']['paging'] = next_json_object['result']['paging']
-        return json_data
+    def __init__(self):
+        super().__init__(json_name='offer-mapping-entries', base_context_name='offerMappingEntries')
 
     def save(self):
-        OfferPattern(json=self.json_data['result']['offerMappingEntries']).save()
+        OfferPattern(json=self.json_data['result'][self.base_context_name]).save()
 
 
 class OfferPrice(Requests):
-    def __init__(self):
-        super().__init__(json_name='/offer-prices')
+    """
+    Класс для получения списка цен на товары и сохранения в БД Price
+    """
 
-    def check_next_page(self, json_data):
-        while 'nextPageToken' in json_data['result']['paging']:  # если страница не последняя, читаем следующую
-            next_page_token = json_data['result']['paging']['nextPageToken']
-            next_json_object = self.get_next_page(next_page_token)
-            json_data['result']['offers'] += next_json_object['result']['offers']
-            json_data['result']['paging'] = next_json_object['result']['paging']
-        return json_data
+    def __init__(self):
+        super().__init__(json_name='offer-prices', base_context_name='offers')
 
     def save(self):
-        PricePattern(json=self.json_data['result']['offers']).save()
+        PricePattern(json=self.json_data['result'][self.base_context_name]).save()
