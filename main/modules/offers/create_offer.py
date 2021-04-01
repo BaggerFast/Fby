9eede533393c5223f3069a29cd0031acffb1dbdb
Daniil Aleksandrov <riosha3@gmail.com>
2021-04-01 22:48:37 +0300

    fix errors

--- main/modules/offers/create_offer.py
+++ main/modules/offers/create_offer.py
@@ -13,7 +13,7 @@ class CreateOfferView(LoginRequiredMixin, View):
     """отображение каталога"""
     context = {'title': 'Create_offer', 'page_name': 'Создать товар'}
 
-    def preinit(self, request):
+    def pre_init(self, request):
         self.context['navbar'] = get_navbar(request)
         self.context['content'] = request.GET.get('content', 'info')
         correct_content = ['info', 'accommodation']
@@ -24,24 +24,25 @@ class CreateOfferView(LoginRequiredMixin, View):
         else:
             raise Http404()
 
-    def endit(self):
+    def end_it(self):
         self.context['forms'] = self.form.get_for_context()
 
     def post(self, request):
-        self.preinit(request=request)
+        self.pre_init(request=request)
         id = int(request.GET.get('id', -1))
-        self.form.get_post(disable=True, request=request.POST)
         if id < 0:
             offer = Offer.objects.create(user=request.user)
             id = offer.id
             self.context['id'] = id
         else:
             offer = Offer.objects.get(id=id)
+        self.form.get_post(disable=True, request=request.POST)
+        self.form.get_models_classes(key1={'id': offer.id}, key2={'offer': offer})
         if self.form.is_valid():
             print('VALID')
-            self.form.save()
             messages.success(request, 'Товар добавлен')
             self.context['disable'] = True
+            self.form.save()
         else:
             self.form.get_post(disable=False, request=request.POST)
             offer.delete()
@@ -55,11 +56,11 @@ class CreateOfferView(LoginRequiredMixin, View):
         return render(request, Page.product_card, self.context)
 
     def get(self, request):
-        self.preinit(request=request)
+        self.pre_init(request=request)
         self.context['create'] = True
         self.form.get_models_classes()
         self.form.get_clear(disable=False)
-        self.endit()
+        self.end_it()
         if int(request.GET.get('id', -1)) >= 0 and self.context['content'] != 'accommodation':
             return redirect(reverse('create_offer')+'?content=accommodation&id='+str(request.GET.get('id', -1)))
         return render(request, Page.product_card, self.context)
