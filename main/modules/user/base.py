24a9885f9a7a94b4b371e9a91c7369906b3da08a
Vladimir Vartanyan <vova.vartanian@bk.ru>
2021-04-09 23:48:41 +0300

    Total html refactoring

--- main/modules/user/base.py
+++ main/modules/user/base.py
@@ -7,7 +7,7 @@ class BaseView(FormView):
 
     def get(self, request, *args, **kwargs):
         self.context['navbar'] = get_navbar(request)
-        self.context['forms'] = self.get_context_data()['form']
+        self.context['form'] = self.get_context_data()['form']
         return self.render_to_response(self.context)
 
     def form_invalid(self, form):
