542854aaa586f02f3312c8b3b0622273ca4fef3a
Daniil Aleksandrov <riosha3@gmail.com>
2021-04-19 00:04:45 +0300

    refactor urls

--- fby_market/additional_url/catalogue.py
+++ fby_market/additional_url/catalogue.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from main.modules import *
-# from main.view import OfferEdit
 
 urlpatterns = [
     path('', CatalogueView.as_view(), name='catalogue_list'),
     path('create/', CreateOfferView.as_view(), name="create_offer"),
     path('<int:pk>/', ProductPageView.as_view(), name='offer_by_sku'),
-    # path('<id>/', OfferDetails.as_view(), name='offer_by_sku'),
-    # path('<shopSku>/edit/', OfferEdit.as_view(), name='offer_by_sku_edit')
 ]
--- fby_market/urls.py
+++ fby_market/urls.py
@@ -18,20 +18,18 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
-
 from fby_market import settings
 from main.modules import *
-import fby_market.additional_url.api.api_catalogue as catalogue
-import fby_market.additional_url.api.api_orders as orders
-from main.modules.user.profile import ProfileView, ProfileEditView
+import fby_market.additional_url.catalogue as catalogue
+import fby_market.additional_url.orders as orders
+import fby_market.additional_url.profile as profile
 
 urlpatterns = [
     path('', MainView.as_view(), name="index"),
     path('admin/', admin.site.urls),
     path('catalogue/', include(catalogue)),
     path('orders/', include(orders)),
-    path('profile', ProfileView.as_view(), name="profile"),
-    path('profile/edit', ProfileEditView.as_view(), name="profile_edit"),
+    path('profile/', include(profile)),
     path('register/', MyRegisterFormView.as_view(), name="register"),
     path('login/', MyLoginFormView.as_view(), name='login'),
     path('logout/', login_required(LogoutView.as_view()), name='logout')
