11a88c026dd4b589ecdd4d38e9b7376216808c90
Daniil Aleksandrov <riosha3@gmail.com>
2021-04-27 00:46:21 +0300

    refactor code

--- main/tests/Cookie.py
+++ main/tests/Cookie.py
@@ -4,28 +4,30 @@ from django.urls import reverse
 from selenium import webdriver
 from django.conf import settings 
 from fby_market.settings import Base_URl, Testing_User_Prarmiters
-class CookieCreate():
-    "Создаёт cookie файл сессии с залогинииым пользователем к странице предастовляющей доступ только авторизованым поользователям"
+
+
+class CookieCreate:
+    """
+    Создаёт cookie файл сессии с авторизованным пользователем к странице предоставляющей
+    доступ только авторизованным пользователям
+    """
 
     def __init__(self, driver, client):
         self.driver = driver
         self.client = client
 
-
     def createCookie(self, driver):
-        self.client.login(username = Testing_User_Prarmiters["name"], password = Testing_User_Prarmiters["password"])
+        self.client.login(username=Testing_User_Prarmiters["name"], password=Testing_User_Prarmiters["password"])
         cookie = self.client.cookies['sessionid']
         self.driver.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
         self.driver.refresh()
 
-
     def createCookieByName(self, url_name):
-        self.driver.get("http://127.0.0.1:8000/" + reverse(url_name))
+        self.driver.get(Base_URl + reverse(url_name))
         self.createCookie(self.driver)
 
-
     def createCookieByURl(self, url):    
-        self.driver.get("http://127.0.0.1:8000/" + url)
+        self.driver.get(Base_URl + url)
         self.createCookie(self.driver)
         
 
--- main/tests/test_commodity_transactions.py
+++ main/tests/test_commodity_transactions.py
@@ -8,11 +8,19 @@ from django.conf import settings
 from fby_market.settings import Base_URl
 from main.tests.Cookie import CookieCreate
 from main.tests.Tests_valeus import Commodity_transactios_test_valeus as CTT_valeus
-#python manage.py dumpdata --exclude auth.permission --exclude contenttypes --indent=4 > main\tests\fixtures\tmp_data.json
-'''тесты на операци производимые с товарами'''
+
+"""
+тесты на операци производимые с товарами
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes --indent=4 > main\tests\fixtures\tmp_data.json
+"""
+
+
+def add_id(data: list):
+    return [f'id_{value}' for value in data]
+
 
 def form_values_placeholder(id_list, valeus):
-    '''заполняет объекты формы по предоставленному списку id значениями из списка valeus'''
+    """заполняет объекты формы по предоставленному списку id значениями из списка valeus"""
 
     for id, valeu in izip(id_list, valeus):
         element = self.driver.find_element_by_id(id)
@@ -22,8 +30,9 @@ def form_values_placeholder(id_list, valeus):
         else:
            element.send_keys(valeu)
 
+
 class testLoadingСommodity(LiveServerTestCase):
-    '''тест на проверку загрузки товаров'''
+    """тест на проверку загрузки товаров"""
     fixtures = ['tmp_data.json']
 
     def setUp(self):
@@ -34,11 +43,13 @@ class testLoadingСommodity(LiveServerTestCase):
     def tearDown(self):
         self.driver.quit()
 
-
     def testLoadingСommodity(self):
-        '''перходит на страницу загрузки каталога и обновляет каталог после чего проверят наличие блока каталога в ином случае выдаёт исключение'''
+        """
+        переходит на страницу загрузки каталога и обновляет каталог после чего проверят наличие блока каталога в ином
+        случае выдаёт исключение
+        """
         self.Cookiecreater.createCookieByName("catalogue_list")
-        self.driver.get("http://127.0.0.1:8000/" + reverse("catalogue_list"))
+        self.driver.get(Base_URl + reverse("catalogue_list"))
         element = self.driver.find_element_by_id("button_loader")
         element.click()
         try:
@@ -46,30 +57,29 @@ class testLoadingСommodity(LiveServerTestCase):
         except exceptions.NoSuchElementException:
             return False
 
+
 class testCreateCommodity(LiveServerTestCase):
-    '''тесты на проверку создания товара'''
+    """тесты на проверку создания товара"""
     fixtures = ['tmp_data.json']
 
     def tearDown(self):
         self.driver.quit()
-   
 
     def testCreateCommodity(self):
-        '''перходит на страницу загрузки каталога Заполняет форму значениями проверяет переход на catalogue'''
+        """переходит на страницу загрузки каталога Заполняет форму значениями проверяет переход на catalogue"""
         self.test_user = Client()
         self.driver = webdriver.Chrome(executable_path=binary_path)
         self.Cookiecreater = CookieCreate(self.driver, self.test_user)
         self.Cookiecreater.createCookieByName("create_offer")
-        self.driver.get("http://127.0.0.1:8000/" + reverse("create_offer"))
-        ID_lst_first = [
-            "id_name", "id_category", "id_vendor", "id_vendorCode", "id_manufacturer", "id_description", "id_url", "id_barcode", "id_code",
-            "id_ShelfLife-timePeriod", "id_ShelfLife-timeUnit", "id_ShelfLife-comment", "id_LifeTime-timePeriod", "id_LifeTime-timeUnit", "id_LifeTime-comment", 
-            "id_GuaranteePeriod-timePeriod", "id_GuaranteePeriod-timeUnit", "id_GuaranteePeriod-comment", "id_length", " id_width", "id_height", "id_weight",
-            "id_transportUnitSize", "id_minShipment", "id_quantumOfSupply", "id_deliveryDurationDays", "id_boxCount",
-                    ]
-        ID_list_second = [
-            "id_currencyId", "id_discountBase", "id_value", "id_vat", "id_availability",
-                            ]
+        self.driver.get(Base_URl + reverse("create_offer"))
+        ID_lst_first = add_id([
+            "name", "category", "vendor", "vendorCode", "manufacturer", "description", "url", "barcode", "id_code",
+            "ShelfLife-timePeriod", "ShelfLife-timeUnit", "ShelfLife-comment", "LifeTime-timePeriod",
+            "LifeTime-timeUnit", "LifeTime-comment", "GuaranteePeriod-timePeriod", "GuaranteePeriod-timeUnit",
+            "GuaranteePeriod-comment", "length", "width", "height", "weight", "transportUnitSize", "minShipment",
+            "quantumOfSupply", "deliveryDurationDays", "id_boxCount",
+                    ])
+        ID_list_second = add_id(["id_currencyId", "id_discountBase", "id_value", "id_vat", "id_availability"])
         for TestValeus in TestCreateCommodityValeus:
            with self.setUp():
                 form_values_placeholder(ID_list_second, CTT_valeus.TestValeus[0])
