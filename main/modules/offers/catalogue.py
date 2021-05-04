e6d267a26a0cc4f3dff20d51338c242834566471
Egorov Alexander <coder.channel48@gmail.com>
2021-05-04 16:00:35 +0300

    Clean up search algorithm

--- main/modules/offers/catalogue.py
+++ main/modules/offers/catalogue.py
@@ -1,3 +1,4 @@
+import itertools
 from django.shortcuts import render
 from django.http import HttpResponse
 from main.models_addon import Offer, Url
@@ -9,44 +10,45 @@ from main.ya_requests import OfferList, OfferPrice
 class CatalogueView(BaseView):
     context = {'title': 'Catalogue', 'page_name': 'Каталог'}
     models_to_save = [OfferList, OfferPrice]
-    table = ["Название", "SKU", "Категория", "Продавец", "Картинка"]
+    fields = ['name', 'description', 'shopSku', 'category', 'vendor']
+    table = ['Название', 'SKU', 'Категория', 'Продавец', 'Картинка']
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.filtration = Filtration({
-            "vendor": "Торговая марка",
-            "category": "Категория",
-            "availability": "Планы по поставкам",
+            'vendor': 'Торговая марка',
+            'category': 'Категория',
+            'availability': 'Планы по поставкам',
         })
+
+    def search_algorithm(self, keywords, objects):
+        if not len(keywords):
+            return objects
+        scores = {}
+        for item, keyword, field in itertools.product(objects, keywords, self.fields):
+            attr = getattr(item, field)
+            if attr is not None and keyword in str(attr).lower():
+                if item not in scores:
+                    scores[item] = 0
+                scores[item] += 1
+                break
+        return sorted(scores, key=scores.get, reverse=True)
 
     def reformat_offer(self, offer, filter_types) -> list:
         def offer_search(offers) -> list:
-            def search_algorithm():
-                if not len(keywords):
-                    return objects
-                scores = {}
-                for item in objects:
-                    for keyword in keywords:
-                        for field in fields:
-                            attr = getattr(item, field)
-                            if attr is not None and keyword in str(attr).lower():
-                                if item not in scores:
-                                    scores[item] = 0
-                                scores[item] += 1
-                                break
-                return sorted(scores, key=scores.get, reverse=True)
-            search = self.request.GET.get('input', '').lower()
-            fields = ['name', 'description', 'shopSku', 'category', 'vendor']
-            keywords = search.strip().split()
+            keywords = self.request.GET.get('input', '').lower().strip().split()
             filters = self.filtration.filters_from_request(self.request, filter_types)
+
             objects = self.filtration.filter_items(offers, filters)
-            objects = search_algorithm()
-            was_searching_used = len(search) != 0
-            for filter_value in filters.values():
-                if filter_value and len(filter_value) != 0:
+            objects = self.search_algorithm(keywords, objects)
+
+            was_searching_used = len(keywords) != 0
+            if not was_searching_used:
+                filter_values = [j for sub in filters.values() for j in sub]
+                if len(filter_values):
                     was_searching_used = True
-                    break
-            self.context_update({'search': was_searching_used, 'count': len(objects)})
+
+            self.context_update({'search': was_searching_used})
             return objects
         return offer_search(offer)
 
