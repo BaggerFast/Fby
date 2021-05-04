115e6fc94994b7fe61aafad1cedca0f0cf92fc45
Viacheslav Leshchenko <slaval.125.sl@gmail.com>
2021-05-04 14:48:40 +0300

    add csv_export.py

---
+++ main/view/csv_export.py
@@ -0,0 +1,22 @@
+import csv
+import io
+from typing import List
+
+from django.db.models import QuerySet
+
+
+def export_qs_to_csv(query_set: QuerySet, field_names: List[str] = None, exclude=True):
+    if not field_names:
+        field_names = ["id"]
+
+    meta = query_set.model._meta
+
+    field_names_ = {field.name: field.verbose_name for field in meta.fields if (field.name in field_names) != exclude}
+
+    with io.open(f"{meta}.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(field_names_.values())
+
+        for obj in query_set:
+            writer.writerow([getattr(obj, field) for field in field_names_.keys()])
