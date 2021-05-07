017736cacb43a070285c526b8873412cd8bb598d
PavelNikolaichev <pavelnikolaichev@gmail.com>
2021-05-07 20:34:54 +0300

    Implemented Pandas-loading from DB and drawing graphs

--- main/modules/analytics/summary.py
+++ main/modules/analytics/summary.py
@@ -1,17 +1,44 @@
+"""Модуль для рендера страницы аналитики"""
 from django.shortcuts import render
 from django.views.generic.base import View
 import plotly.graph_objects as go
+import pandas as pd
+import plotly.offline as opy
 import plotly.express as px
+from main.models_addon.ya_market.offer.base import Offer
 
 from main.view import *
 
 
+def get_pie_figure():
+    """
+    Создание круговой диаграммы
+    :return: plot class object
+    """
+    offers = get_data_from_db()
+    fig = go.Figure(data=[go.Pie(labels=offers.columns, values=offers.value_counts())])
+    return opy.plot(fig, auto_open=False, output_type='div')
+
+
+def get_data_from_db():
+    """
+    Функция для получения данных из БД
+    :return: Pandas.DataFrame
+    """
+    offers = Offer.objects.all().values('transportUnitSize', 'user')
+    offers = pd.DataFrame(offers)
+    return offers
+
+
 class SummaryView(View):
-    """отображение главной страницы"""
-    context = {'title': 'Summary'}
+    """Отображение главной страницы"""
+    context = {
+        'title': 'Summary',
+    }
 
     def get(self, request):
         self.context['navbar'] = get_navbar(request)
+        self.context['graph'] = get_pie_figure()
         return render(request, Page.summary, self.context)
 
 
@@ -25,7 +52,7 @@ class SummaryView(View):
 
 
 def pie(labels=None, values=None):
-    """создание круговой диаграммы"""
+    """Создание круговой диаграммы"""
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
     fig.show()
 
@@ -47,6 +74,6 @@ def pie(labels=None, values=None):
 
 
 def time_series(df, y_axis_name, range_bottom, range_top):
-    """создание диаграммы за периуд времени"""
+    """Создание диаграммы за периуд времени"""
     fig = px.line(df, x='Date', y=y_axis_name, range_x=[range_bottom, range_top])
     fig.show()
