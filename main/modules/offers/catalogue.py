6069469344c51efb1c514eabe1e0a92634a239f4
Alex Kochnov <kipperto@gmail.com>
2021-05-12 18:54:47 +0300

    Add checkbox for view-class

--- main/modules/offers/catalogue.py
+++ main/modules/offers/catalogue.py
@@ -1,3 +1,5 @@
+import time
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from main.models_addon import Offer
@@ -5,12 +7,25 @@ from main.modules.base import BaseView
 from main.view import get_navbar, Page, Filtration
 from main.ya_requests import OfferList, OfferPrice
 
+from django import forms
+from django.forms.formsets import formset_factory
+
+
+class CheckboxForm(forms.Form):
+    box = forms.BooleanField()
+
+
+class CheckBoxSet:
+    def __init__(self, extra):
+        self.article_formset = formset_factory(CheckboxForm, extra=extra)
+        self.formset = self.article_formset()
+
 
 class CatalogueView(BaseView):
     context = {'title': 'Catalogue', 'page_name': 'Каталог'}
     models_to_save = [OfferList, OfferPrice]
     fields = ['name', 'description', 'shopSku', 'category', 'vendor']
-    table = ['Название', 'SKU', 'Категория', 'Продавец']
+    table = ['', 'Название', 'SKU', 'Категория', 'Продавец']
     filtration = Filtration({
         "vendor": "Торговая марка",
         "category": "Категория",
@@ -18,16 +33,29 @@ class CatalogueView(BaseView):
     })
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        return self.save_models(request=request)
+        if 'button_loader' in request.POST:
+            return self.save_models(request=request)
+        elif 'checkbox' in request.POST:
+            numbers = [int(box.split('-')[1]) for box in list(dict(request.POST).keys())[1:-1]]
+            offers = Offer.objects.filter(user=request.user)
+            filter_types = self.filtration.get_filter_types(offers)
+            sorted_obj = self.sort_object(offers, filter_types)
+            for number in numbers:
+                sorted_obj[number].delete()
+        return self.get(request)
 
     def get(self, request: HttpRequest) -> HttpResponse:
         offers = Offer.objects.filter(user=request.user)
         filter_types = self.filtration.get_filter_types(offers)
+        sorted_obj = self.sort_object(offers, filter_types)
+        formset = CheckBoxSet(len(sorted_obj)).formset
         local_context = {
             'navbar': get_navbar(request),
-            'offers': self.sort_object(offers, filter_types),
+            'offers': sorted_obj,
             'table': self.table,
             'filter_types': filter_types.items(),
+            'formset': formset,
+            'table_lines': [{'form': form, 'offer': offer} for form, offer in zip(formset, sorted_obj)]
         }
         self.context_update(local_context)
         return render(request, Page.catalogue, self.context)
