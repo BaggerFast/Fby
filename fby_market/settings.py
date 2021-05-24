04a97cdf44503e2dd95027dda977830ef1664462
Daniil Aleksandrov <riosha3@gmail.com>
2021-05-24 23:49:58 +0300

    add static root settings.py

--- fby_market/settings.py
+++ fby_market/settings.py
@@ -136,6 +136,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
