e88d9c663c07f3a3562e7d2a8d7ef947cca9fc91
Николайчев Павел <pavelnikolaichev@gmail.com>
2021-05-29 18:46:40 +0000

    Implementing CI/CD

---
+++ ci/pylint/runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run pylint
+pylint --load-plugins pylint_django $(ls -d */) | tee pylint.txt
+
+# get badge
+mkdir public
+score=$(sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p' pylint.txt)
+anybadge --value=$score --file=public/pylint.svg pylint
+echo "Pylint score was $score"
+
+#cleanup
+rm pylint.txt
+
+exit 0
