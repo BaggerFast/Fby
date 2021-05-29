e4521b31d19f61fde8589aabb4448ccac7cb504d
Daniil Sushko <dansus204@yandex.ru>
2021-05-29 22:36:09 +1100

    Add navbar and page title

--- main/modules/analytics/stats.py
+++ main/modules/analytics/stats.py
@@ -1,13 +1,32 @@
+import datetime
 from django.shortcuts import render
 from django.views.generic.base import View
-from main.view import *
+from main.models_addon import Offer
+from main.modules.base import BaseView
+from main.models_addon.ya_market.order.base import Order, Item, Warehouse
+from main.view import Page, get_navbar
 
 
-class StatsView(View):
-    """Отображение страницы"""
+def offers_for_last_14_days(included_statuses, user):
+    return Offer.objects.filter(
+
+    )
+
+
+class StatsView(BaseView):
+    """Отображение информации о проданных тоарах"""
     context = {
         'title': 'Статистика',
+        'page_name': 'Статистика продаж',
     }
 
+    def context_update(self, data: dict):
+        self.context = {**data, **self.context}
+
     def get(self, request):
+        local_context = {
+            'navbar': get_navbar(request),
+        }
+        self.context_update(local_context)
         return render(request, Page.stats, self.context)
+
