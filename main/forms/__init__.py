5adaa51238bf5376f1128de9114fea5b44e1903a
Daniil Aleksandrov <riosha3@gmail.com>
2021-05-30 11:10:20 +0300

    merge

--- main/forms/__init__.py
+++ main/forms/__init__.py
@@ -1,4 +1,3 @@
 from .user import UserLoginForm, UserRegistrationForm
 from .offer import ShelfLifeForm, LifeTimeForm, GuaranteePeriodForm, OfferForm, UrlForm, \
     CommodityCodeForm, PriceForm, AvailabilityForm, WeightDimensionForm, BarcodeForm, ManufacturerCountryForm
-from .feedback import FeedbackForm
--- main/modules/index.py
+++ main/modules/index.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic.base import View
 from main.view import Navbar, Page
@@ -6,6 +7,11 @@ from main.view import Navbar, Page
 class MainView(View):
     """отображение главной страницы"""
     context = {'title': 'Главная'}
+
+    def post(self, request):
+        messages.success(request, 'Информация отправлена')
+        self.context['navbar'] = Navbar(request).get()
+        return render(request, Page.index, self.context)
 
     def get(self, request):
         self.context['navbar'] = Navbar(request).get()
